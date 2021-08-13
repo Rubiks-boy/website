b2560ebc3ecd5625fe7c830746cdb9680d15540c
Adam Walker <adamwalker567123@gmail.com>
2021-08-13 16:34:29 -0700

    start of typewriter showcase thing

---
+++ src/components/Showcase/index.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./index.css";
+
+const Showcase = () => {
+  return (
+    <div className="showcase start">
+      <h1 className="typewriter line0">Showcase</h1>
+
+      <p className="typewriter line1">Line 1</p>
+      <p className="typewriter line2">Line 2 is longer</p>
+      <p className="typewriter line3">Line 3 is even longer</p>
+    </div>
+  );
+};
+
+export default Showcase;
